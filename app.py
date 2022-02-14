030d922b1c34b03078a27bf395ec2e7e0f71d6ef
Enzo <Zirotti32@gmail.com>
2022-02-13 18:38:26 -0700

    Pushing into 9.5.3

--- app.py
+++ app.py
@@ -1,8 +1,57 @@
-from flask import Flask
+
+#################################################
+# Application create by Emmanuel Martinez
+# from Module 9
+#################################################
+# Import the Flask Dependency
+#################################################
+
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+#################################################
+# Set Up the Database and Flask
+# Module 9.5.1
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+#################################################
+# Create a New Flask App Instance
+#################################################
 app = Flask(__name__)
-@app.route('C:\Users\zirot\Classwork\Surfs_up/')
 
+#################################################
+# Create the Welcome Route
+# Module 9.5.2
+#################################################
 
-def hello_world():
-    return 'Hello world'
-
+@app.route('/')
+def welcome():
+    return(
+        f"Welcome to the Hawaii Climate Analysis API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/temp/start/end"
+    )
